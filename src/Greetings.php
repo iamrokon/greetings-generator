d236b5267bd7f0643829fa39e6336e255cdc0825
rokon <rokonuzzamancse@gmail.com>
2025-06-14 10:48:48 +0600

    Second version added

--- src/Greetings.php
+++ src/Greetings.php
@@ -4,10 +4,21 @@ namespace Rokon\GreetingsGenerator;
 
 class Greetings
 {
-    private static $messages = ['Hi', 'Hello', 'How are you'];
+    // private static $messages = ['Hi', 'Hello', 'How are you'];
+    private static $greetings = [
+        'bn' => ['হ্যালো', 'কেমন আছেন', 'আসসালামু আলাইকুম'],
+        'en' => ['Hi', 'Hello', 'How are you'],
+        'ar' => ['مرحبا', 'كيف حالك', 'السلام عليكم'],
+        'tr' => ['Merhaba', 'Nasılsın?', 'Selamlar'],
+        'es' => ['Hola', '¿Cómo estás?', 'Saludos'],
+    ];
 
-    public static function greet()
+    public static function greet(?string $language = null): string
     {
-        return self::$messages[array_rand(self::$messages)];
+        $selectedLanguage = $language ?? array_rand(self::$greetings);
+        $selectedGreetings = self::$greetings[$selectedLanguage] ?? self::$greetings['en'];
+
+        return $selectedGreetings[array_rand($selectedGreetings)];
+        // return self::$messages[array_rand(self::$messages)];
     }
 }
--- tests/GreetingsTest.php
+++ tests/GreetingsTest.php
@@ -3,6 +3,20 @@
 use Rokon\GreetingsGenerator\Greetings;
 
 it('test generates random greetings', function () {
-    $messages = ['Hi', 'Hello', 'How are you'];
+    // $messages = ['Hi', 'Hello', 'How are you'];
+    $messages = [
+        'হ্যালো', 'কেমন আছেন', 'আসসালামু আলাইকুম',
+        'Hi', 'Hello', 'How are you',
+        'مرحبا', 'كيف حالك', 'السلام عليكم',
+        'Merhaba', 'Nasılsın?', 'Selamlar',
+        'Hola', '¿Cómo estás?', 'Saludos',
+    ];
     expect(Greetings::greet())->toBeIn($messages);
+});
+
+it('test generates greetings in local language', function () {
+    $messages = ['مرحبا', 'كيف حالك', 'السلام عليكم'];
+
+    expect(Greetings::greet('ar'))->toBeIn($messages);
+
 });
